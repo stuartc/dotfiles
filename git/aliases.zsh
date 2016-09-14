2642e43dd9c2283345b93b64381c462c0fa2338c
Stuart Corbishley <corbish@gmail.com>
2016-09-14 13:51:11 +0200

    Add [32m2016-06-04 15:39:10 +0200 [34m3 months ago[m	master alias for showing age of branches

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -18,3 +18,4 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias gfrom='git fetch && git rebase origin/master'
+alias gage='for k in `git branch|perl -pe s/^..//`;do echo -e `git show --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" $k|head -n 1`\\t$k;done|sort -r'
